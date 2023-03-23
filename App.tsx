96fa94456e044eeb34416a17c1a24b0efd266f19
Chance <chancesmithb@gmail.com>
2023-03-23 15:37:59 -0500

    📦  FEAT: [App, CustomerList] read cache and read offline status

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
+import NetInfo from "@react-native-community/netinfo";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { API_SECRET, API_URL } from "@env";
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { CustomerList } from "./components/CustomerList";
 import { CONTAINER } from "./styles";
+import { useEffect, useState } from "react";
 
 if (!API_URL || !API_SECRET) {
   alert(
     "No API_URL or API_SECRET set. It's possible this project is missing the /.env file in the root of the project."
   );
 }
+
+// setup for offline caching, and query queuing
 
 const client = new ApolloClient({
   uri: API_URL,
@@ -20,10 +24,42 @@ const client = new ApolloClient({
 });
 
 export default function App() {
+  const [isOnline, setIsOnline] = useState(true);
+  const [ignoreErrorTimeout, setIgnoreErrorTimeout] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIgnoreErrorTimeout(false);
+    }, 3000);
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsOnline(!!state?.isInternetReachable);
+    });
+    return () => {
+      clearTimeout(timer);
+      unsubscribe();
+    };
+  }, []);
+
+  const OfflineBar = () => (
+    <View
+      style={{
+        position: "absolute",
+        bottom: 5,
+        backgroundColor: "#777",
+        width: "100%",
+        paddingVertical: 10,
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ color: "#fff" }}>Offline</Text>
+    </View>
+  );
+
   return (
     <ApolloProvider client={client}>
       <View style={CONTAINER}>
         <StatusBar style="auto" />
+        {!isOnline && !ignoreErrorTimeout ? <OfflineBar /> : null}
         <CustomerList />
       </View>
     </ApolloProvider>
--- components/CustomerList.tsx
+++ components/CustomerList.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
-import { Text, View } from "react-native";
+import { useQuery, gql, useApolloClient } from "@apollo/client";
+import { Button, Text, View } from "react-native";
 import { HEADING, TEXT } from "../styles";
 
 export const CustomerList = () => {
+  const client = useApolloClient();
+
   const GET_CUSTOMERS = gql`
     query GetCustomers {
       customer {
@@ -19,6 +21,14 @@ export const CustomerList = () => {
   if (loading) return <Text style={TEXT}>Loading...</Text>;
   if (error) return <Text style={TEXT}>Error :( {JSON.stringify(error)}</Text>;
 
+  const { customers } = client.readQuery({
+    query: GET_CUSTOMERS,
+  });
+
+  const handleReadCache = () => {
+    alert(JSON.stringify(customers));
+  };
+
   return (
     <View>
       <Text style={HEADING}>Customers</Text>
@@ -27,6 +37,8 @@ export const CustomerList = () => {
           {customer.first_name} {customer.last_name}
         </Text>
       ))}
+
+      <Button title="Read Cache" onPress={handleReadCache} />
     </View>
   );
 };
